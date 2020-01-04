89b6f0893737abf5b50b5e2edfb2aba82256c5f2
smahapatra <smahapatra@readytech.com>
2020-01-04 02:33:51 +0100

    changed text on feature

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Bye there man!
+          I am running on AWS!
         </p>
         <a
           className="App-link"
